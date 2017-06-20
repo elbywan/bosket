d384126a0cccd9a6c7b7825468c6621658b3a899
Julien Elbaz <julien.elbaz@web-education.net>
2017-06-20 13:02:45 +0200

    typos

--- docs/react/plan/introduction/index.js
+++ docs/react/plan/introduction/index.js
@@ -7,8 +7,8 @@ export default {
     content:
         <p>
             This page covers the <em className="highlight"><a href="https://facebook.github.io/react/" target="_blank" rel="noopener noreferrer">React</a></em> implementation of the <em><a href="../#Introduction">Bosket library</a></em>.<br/>
-            Its goal is to be extremely versatile and flexible, in order to create any tree view with the minimal amount of pain.<br/>
+            Versatile and flexible, Bosket eases the creation and design of tree view components.<br/>
             <br/>
-            You will find below the library <em><a href="#Components">documentation</a></em> and <em><a href="#TreeView#Demo">use cases demos</a></em>.
+            You will find below the <em><a href="#Components">documentation</a></em> and <em><a href="#TreeView#Demo">use cases demos</a></em>.
         </p>
 }
