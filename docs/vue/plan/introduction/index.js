1f7deecf8dd78869af9c2f7d034afa251e72ed92
Julien Elbaz <julien.elbaz@web-education.net>
2017-07-26 15:12:41 +0200

    small layout adjustments

--- docs/vue/plan/introduction/index.js
+++ docs/vue/plan/introduction/index.js
@@ -9,8 +9,10 @@ export default {
                 This page covers the <em class="highlight"><a href="https://vuejs.org" target="_blank" rel="noopener noreferrer">Vue.js</a></em> implementation of the <em><a href="../#Introduction">Bosket library</a></em>.<br/>
                 Versatile and flexible, Bosket eases the creation and design of tree view components.<br/><br/>
                 As a matter of fact <em><a href="https://github.com/elbywan/bosket/tree/master/docs/vue/plan" target="_blank" rel="noopener noreferrer">this whole documentation</a></em> is built around Bosket using <em><a href="https://github.com/elbywan/bosket/blob/master/docs/vue/components/Planner.js" target="_blank" rel="noopener noreferrer">a custom component</a></em> which automatically creates the table of contents and the anchors.<br/>
+                <br/>
+                Multiple use case examples are provided alongside the documentation including the source code.
             </p>
-            <h4 className="center-text">API views implemented with Bosket</h4>
+            <h4 class="center-text">API views implemented with Bosket</h4>
             <IntroductionDemos />
         </div>
 }
